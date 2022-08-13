481e21b41b5d9935f43e6a10159cbcc1359d544c
mn <mn@furzoom.com>
2022-08-13 17:55:50 +0800

    Make 'cat-file' output the file contents to stdout.
    New syntax: "cat-file -t <sha1>" shows the tag, while
    "cat-file <tag> <sha1>" outputs the file contents after
    checking that the supplied tag matches.
    
    Signed-off-by: mn <mn@furzoom.com>

--- cat-file.c
+++ cat-file.c
@@ -10,28 +10,49 @@ int main(int argc, char* argv[]) {
   char type[20];
   void* buf;
   unsigned long size;
-  char tempate[] = "temp_tigg_file_XXXXXX";
-  int fd;
 
-  if (argc != 2 || get_sha1_hex(argv[1], sha1)) {
-    usage("cat-file <sha1>");
+  if (argc != 3 || get_sha1_hex(argv[2], sha1)) {
+    usage("cat-file: cat-file [-t | tagname] <sha1>");
   }
 
   buf = read_sha1_file(sha1, type, &size);
   if (!buf) {
+    fprintf(stderr, "cat-file %s: bad file\n", argv[2]);
     exit(1);
   }
 
-  fd = mkstemp(tempate);
-  if (fd < 0) {
-    usage("unable to create tempfile");
+  if (!strcmp("-t", argv[1])) {
+    buf = type;
+    size = strlen(type);
+    type[size] = '\n';
+    size++;
+  } else if (strcmp(type, argv[1]) != 0) {
+    fprintf(stderr, "cat-file %s: bad tag\n", argv[2]);
+    exit(1);  /* bad tag */
   }
 
-  if (write(fd, buf, size) != size) {
-    strcpy(type, "bad");
-  }
+  while (size > 0) {
+    long ret = write(1, buf, size);
+    if (ret < 0) {
+      if (errno == EAGAIN) {
+        continue;
+      }
+      /* Ignore EPIPE */
+      if (errno == EPIPE) {
+        break;
+      }
 
-  printf("%s: %s\n", tempate, type);
+      fprintf(stderr, "cat-file: %s\n", strerror(errno));
+      exit(1);
+    }
+    if (!ret) {
+      fprintf(stderr, "cat-file: disk full?\n");
+      exit(1);
+    }
+
+    size -= ret;
+    buf += ret;
+  }
 
   return 0;
 }
